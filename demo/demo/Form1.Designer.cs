cd44d7f03df20108a7210bb9815e58f7584adb1d
Trọng Nhân <dotrongnhan15102000@gmail.com>
2022-03-01 11:07:15 +0700

    done login form

--- demo/demo/Form1.Designer.cs
+++ demo/demo/Form1.Designer.cs
@@ -28,13 +28,112 @@ namespace demo
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtusername = new System.Windows.Forms.TextBox();
+            this.txtpassword = new System.Windows.Forms.TextBox();
+            this.btnlogin = new System.Windows.Forms.Button();
+            this.btnexit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(344, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "LOGIN";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(86, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(197, 39);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Username:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(86, 216);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(189, 39);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Password:";
+            // 
+            // txtusername
+            // 
+            this.txtusername.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtusername.Location = new System.Drawing.Point(318, 124);
+            this.txtusername.Name = "txtusername";
+            this.txtusername.Size = new System.Drawing.Size(364, 47);
+            this.txtusername.TabIndex = 3;
+            // 
+            // txtpassword
+            // 
+            this.txtpassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtpassword.Location = new System.Drawing.Point(318, 213);
+            this.txtpassword.Name = "txtpassword";
+            this.txtpassword.PasswordChar = '*';
+            this.txtpassword.Size = new System.Drawing.Size(364, 47);
+            this.txtpassword.TabIndex = 4;
+            // 
+            // btnlogin
+            // 
+            this.btnlogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnlogin.Location = new System.Drawing.Point(318, 312);
+            this.btnlogin.Name = "btnlogin";
+            this.btnlogin.Size = new System.Drawing.Size(155, 48);
+            this.btnlogin.TabIndex = 5;
+            this.btnlogin.Text = "Login";
+            this.btnlogin.UseVisualStyleBackColor = true;
+            this.btnlogin.Click += new System.EventHandler(this.btnlogin_Click);
+            // 
+            // btnexit
+            // 
+            this.btnexit.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnexit.Location = new System.Drawing.Point(526, 312);
+            this.btnexit.Name = "btnexit";
+            this.btnexit.Size = new System.Drawing.Size(156, 48);
+            this.btnexit.TabIndex = 6;
+            this.btnexit.Text = "Exit";
+            this.btnexit.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnexit);
+            this.Controls.Add(this.btnlogin);
+            this.Controls.Add(this.txtpassword);
+            this.Controls.Add(this.txtusername);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtusername;
+        private System.Windows.Forms.TextBox txtpassword;
+        private System.Windows.Forms.Button btnlogin;
+        private System.Windows.Forms.Button btnexit;
     }
 }
 
--- demo/demo/Form1.cs
+++ demo/demo/Form1.cs
@@ -16,5 +16,29 @@ namespace demo
         {
             InitializeComponent();
         }
+
+        private void btnlogin_Click(object sender, EventArgs e)
+        {
+            string tk = txtusername.Text;
+            string mk = txtpassword.Text;
+            if(tk.Equals("") || mk.Equals("")) {
+                MessageBox.Show("Username or password is not allowed!", "Notification");
+                if (tk.Equals("")){
+                    txtusername.Focus();
+                }
+                else {
+                    txtpassword.Focus();
+                }
+            }else if (tk.Equals("nhan") && mk.Equals("123123")) {
+                MessageBox.Show("Login success!","Notification");
+                Home home = new Home();
+                this.Hide();
+                home.Show();
+            }
+            else {
+                MessageBox.Show("Username or password wrong!");
+                txtusername.Focus();
+            }
+        }
     }
 }
